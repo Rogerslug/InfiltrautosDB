5cfd2af486ac36616ea2e580b2631e1330314a4d
Alan Rogelio Lopez Cardenas <rogelio.020496@gmail.com>
2024-09-23 23:25:13 -0600

    Patch: Infiltrautos

--- Infiltrautos.sql
+++ Infiltrautos.sql
@@ -1,31 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 24-09-2024 a las 05:35:26
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `filtros`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `auto`
---
+-- Base de datos: `Infiltrautos`
 
 IF NOT EXISTS (CREATE DATABASE `Infiltrautos`) ;
 
